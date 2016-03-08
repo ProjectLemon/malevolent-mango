c486e18b86339b554cb82ddc1393f97cd81dbe8c
Fredrik Johansson <f95johansson@gmail.com>
2016-03-08 12:10:37 +0100

    Setup angular v2

---
+++ www/js/controllers/LoginController.js
@@ -0,0 +1,26 @@
+app.controller('LoginController',
+  function() {
+    
+  
+  /*
+    var editProject = this;
+    var projectId = $routeParams.projectId,
+        projectIndex;
+ 
+    editProject.projects = projects;
+    projectIndex = editProject.projects.$indexFor(projectId);
+    editProject.project = editProject.projects[projectIndex];
+ 
+    editProject.destroy = function() {
+        editProject.projects.$remove(editProject.project).then(function(data) {
+            $location.path('/');
+        });
+    };
+ 
+    editProject.save = function() {
+        editProject.projects.$save(editProject.project).then(function(data) {
+           $location.path('/');
+        });
+    };
+  */
+});
