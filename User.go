37a96b49fe9c86f46b9234d94d4e32982e88262a
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-03-11 19:06:31 +0100

    Match table cells in user

--- User.go
+++ User.go
@@ -1,8 +1,12 @@
 package main
 
+import "database/sql"
+
 //User provides a datatype for gathering the user information
 //for the current user.
 type User struct {
-    Email string
-    PasswordHash string
+	Email        sql.NullString
+	FullName     sql.NullString
+	PasswordHash sql.NullString
+	Salt         sql.NullString
 }
