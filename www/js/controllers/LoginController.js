86e7cb1f5ca355796c61fc0952b5ac5851d8124e
Fredrik Johansson <f95johansson@gmail.com>
2016-03-11 18:31:56 +0100

    Added login with tokens (Client)

--- www/js/controllers/LoginController.js
+++ www/js/controllers/LoginController.js
@@ -1,26 +1,34 @@
-app.controller('LoginController',
-  function() {
-    
-  
-  /*
-    var editProject = this;
-    var projectId = $routeParams.projectId,
-        projectIndex;
- 
-    editProject.projects = projects;
-    projectIndex = editProject.projects.$indexFor(projectId);
-    editProject.project = editProject.projects[projectIndex];
- 
-    editProject.destroy = function() {
-        editProject.projects.$remove(editProject.project).then(function(data) {
-            $location.path('/');
-        });
-    };
- 
-    editProject.save = function() {
-        editProject.projects.$save(editProject.project).then(function(data) {
-           $location.path('/');
-        });
-    };
-  */
-});
+
+app.controller('LoginController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  $scope.user = {email: '', password: ''};
+  $scope.message = '';
+  $scope.submit = function () {
+    console.log('Posting');
+    $http
+      .post('/login', $scope.user)
+      .then(
+        /* On success */
+        function (response) {
+          /* The response object has these properties:
+
+          • data – {string|Object} – The response body transformed with the transform functions.
+          • status – {number} – HTTP status code of the response.
+          • headers – {function([headerName])} – Header getter function.
+          • config – {Object} – The configuration object that was used to generate the request.
+          • statusText – {string} – HTTP status text of the response.
+          */
+        
+          $window.sessionStorage.token = response.data.token;
+          $scope.message = 'Welcome';
+        },
+        /* On error */
+        function (response) {
+          // Erase the token if the user fails to log in
+          delete $window.sessionStorage.token;
+
+          // Handle login errors here
+          $scope.message = response.data;
+        }
+      );
+  };
+}]);
