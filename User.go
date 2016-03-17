e5e1bb863da9942ea0c7e82affd8d10e1f12fdb9
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-03-17 22:01:48 +0100

    Taking the chance to remove some code

--- User.go
+++ User.go
@@ -9,18 +9,6 @@ type User struct {
 	Salt     string
 }
 
-//NewUser creates a new instance of a user type
-//assigning the provided email address to the address field
-//and zero value for other fields
-func NewUser(email string) *User {
-	user := new(User)
-	user.Email = email
-	user.FullName = ""
-	user.Password = ""
-	user.Salt = ""
-	return user
-}
-
 func (u *User) InDatabase() bool {
 	return (u.FullName != "" && u.Password != "" && u.Salt != "")
 }
