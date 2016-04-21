0d00aa5b70ad4fd157d30e1e6109640d815814db
Fredrik Johansson <f95johansson@gmail.com>
2016-04-21 18:22:15 +0200

    Pdfs are now shown in client

---
+++ www/src/directives/pdfViewer.js
@@ -0,0 +1,37 @@
+/**
+ * pdfViewer shows a pdf and update it when source change
+ * Should have two attributes, pdfs and current. Current is only
+ * a number but pdfs should be an array of pdfs in form of
+ * [{Title: 'title', Path: 'som/path'}, ...]
+ */
+app.directive('pdfViewer', ['$compile', function($compile) {
+  return {
+    restrict: 'E',
+    scope: {
+      pdfs: '@',
+      current: '='
+    },
+    
+    link: function(scope, element, attrs) {
+      
+      var currentElement = element;
+      scope.$parent.$watch(scope.pdfs, function(newValue, oldValue) {
+        var html;
+        if (newValue.length > 0) {
+          var pdf = newValue[scope.current];
+          if (pdf != null) {
+            html = '<embed src="'+pdf.Path+'" type="application/pdf">'
+            
+          } else {
+            html = '<p>No pdf</p>'
+          } 
+        } else {
+          html = '<p>No pdf</p>'
+        }
+        var e = $compile(html)(scope);
+        currentElement.replaceWith(e);
+        currentElement = e;
+      });
+    }
+  };
+}]);
